029d7bce3f0c544c393bd1ecdcd044209aa2d556
Nils Deckert <nils@deckert.dev>
2021-12-06 16:14:49 +0100

    shortened code for 5-2

--- 2021/Day 5 - 2/main.py
+++ 2021/Day 5 - 2/main.py
@@ -7,11 +7,8 @@ def parse_input(file: str) -> list[tuple]:
     with open(file, "r") as f:
         for line in f:
             num_a, num_b = line.strip().split(" -> ")
-            x_a, y_a = [int(a) for a in num_a.split(",")]
-            x_b, y_b = [int(b) for b in num_b.split(",")]
-
-            a = np.array([x_a, y_a])
-            b = np.array([x_b, y_b])
+            a = np.array([int(a) for a in num_a.split(",")])
+            b = np.array([int(b) for b in num_b.split(",")])
 
             lines.append((a, b))
     return lines
@@ -26,22 +23,15 @@ def count_occurrences(l: list) -> int:
 def main(file: str) -> int:
     lines = parse_input(file)
     hit = []
+    # returns 1 if a < b; -1 if a > b; 0 else
+    f = lambda a, b: int(a < b) - int(a > b)
     for line in lines:
 
-        x_a, y_a = line[0][0], line[0][1]
-        x_b, y_b = line[1][0], line[1][1]
+        (x_a, y_a), (x_b, y_b) = line
 
-        # Either x or y value is the same, so use np.arange for the other value to get all points inbetween
-        if x_a == x_b:
-            a, b = y_a, y_b
-        elif y_a == y_b:
-            a, b = x_a, x_b
-
-        # Make sure np.arange works properly
-        if b < a:
-            b, a = a, b
-
-        z = np.arange(a, b + 1, 1, dtype=int)
+        # Either x or y value is the same, so use range() for the other value to get all points inbetween
+        a, b = [(y_a, y_b), (x_a, x_b)][int(y_a == y_b)]
+        z = range(a, b + f(a, b), f(a, b))
 
         if x_a == x_b:
             hit += [(x_a, y_i) for y_i in z]
@@ -49,15 +39,8 @@ def main(file: str) -> int:
             hit += [(x_i, y_a) for x_i in z]
         else:  # 45 Degree angle
 
-            # flip 'direction' if x/y value of b is smaller than x/y of a
-            x_n, y_n = 1, 1
-            if y_b < y_a:
-                y_n = -1
-            if x_b < x_a:
-                x_n = -1
-
-            y = range(y_a, y_b + y_n, y_n)
-            x = range(x_a, x_b + x_n, x_n)
+            y = range(y_a, y_b + f(y_a, y_b), f(y_a, y_b))
+            x = range(x_a, x_b + f(x_a, x_b), f(x_a, x_b))
 
             hit += [(x_i, y_i) for x_i, y_i in zip(x, y)]
 
